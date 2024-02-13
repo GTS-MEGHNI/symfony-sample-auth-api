3cf25e67afd40fd69f00c0bff7309e4bb0a01f10
GTS-MEGHNI <meghniucrpg@live.fr>
2024-02-13 18:18:02 +0100

    Implement user registration endpoint

--- src/Services/RegisterService.php
+++ src/Services/RegisterService.php
@@ -3,13 +3,26 @@
 namespace App\Services;
 
 use App\DTO\RegisterRequest;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterService extends AuthService
 {
+    public function __construct(public UserPasswordHasherInterface $hasher,
+                                public EntityManagerInterface $manager)
+    {
+
+    }
 
     public function registerUser(RegisterRequest $request)
     {
-
+        $this->user = new User();
+        $this->user->setEmail($request->email);
+        $this->user->setUsername($request->username);
+        $this->user->setPassword($this->hasher->hashPassword($this->user, $request->password));
+        $this->manager->persist($this->user);
+        $this->manager->flush();
     }
 
 }
